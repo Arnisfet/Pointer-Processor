11720453628d2a89c0db70171ed73630f4be852f
Viktor Koliukh <v.koliuh@hybrid.ai>
2024-08-27 13:32:13 +0300

    Stylish buttons were added

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import {Header} from './components/Header.jsx';
+import 'bootstrap/dist/css/bootstrap.min.css'; // Import Bootstrap globally
 import '../public/styles/header.css'
 
 
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,13 +1,20 @@
 import React from 'react';
+import Button from 'react-bootstrap/Button';
 
 export function Header() {
   return (
-    <div className="top-header">
-      <h1>Point Processor | Audience uploader</h1>
-    </div>
+    <>
+      <div className="top-header">
+        <h3>Point Processor | Audience Uploader</h3>
+      </div>
+      {/*Pay attention!! Two different divs*/}
+      <div className="down-header">
+        <h2>Pointer Processor | Audience Uploader</h2>
+        <Button variant="outline-dark">First Option</Button>
+        <Button variant="outline-dark">Second Option</Button>
+        <Button variant="outline-dark">Third Option</Button>
+        <Button variant="outline-dark">Fourth Option</Button>
+      </div>
+    </>
   );
 }
-//
-// export function DownHeader() {
-//   return();
-// }
